58dd2547c79e9711846dafa4d704308d46b5b608
Lavish <64689132+lavish440@users.noreply.github.com>
2023-07-30 11:00:46 +0530

    Removed the filename argument passed to epg.py

--- epg.sh
+++ epg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 termux-notification --ongoing --id "epg" --group "group" -c "EPG generation started"
 cd ~/git/epg
-python epg.py epg.xml.gz
+python epg.py
 termux-notification --id "epg" --group "group" -c "EPG Generation Finished"
 git add epg.xml.gz
 git commit -m "Updated EPG"
